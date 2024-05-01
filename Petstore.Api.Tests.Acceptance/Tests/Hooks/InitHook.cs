97a6e0f043ba603ef385e192743fe1c7cc118ff7
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-01 21:23:09 +0200

    add InitHook

---
+++ Petstore.Api.Tests.Acceptance/Tests/Hooks/InitHook.cs
@@ -0,0 +1,33 @@
+﻿using BoDi;
+using Microsoft.Extensions.Configuration;
+
+namespace Petstore.Api.Tests.Acceptance.Tests.Hooks;
+
+public class InitHook
+{
+    private IObjectContainer _objectContainer;
+
+    public InitHook(IObjectContainer objectContainer)
+    {
+        _objectContainer = objectContainer;
+    }
+
+    [BeforeScenario]
+    public void RegisterHttpClient()
+    {
+        var config = LoadConfiguration();
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(config["Petstore.Api:BaseUrl"]!)
+        };
+
+        _objectContainer.RegisterInstanceAs(httpClient);
+    }
+
+    private static IConfiguration LoadConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+    }
+}
