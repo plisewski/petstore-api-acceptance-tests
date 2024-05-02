2cb9430f7b7b7dc5ffc5f96cba6a65f169ec8a3c
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-02 15:51:12 +0200

    update test initialization hook

---
+++ Petstore.Api.Tests.Acceptance/Tests/Hooks/TestInitializationHook.cs
@@ -0,0 +1,26 @@
+﻿using BoDi;
+using Petstore.Api.Tests.Acceptance.ApiServices;
+using Petstore.Api.Tests.Acceptance.RestClient;
+
+namespace Petstore.Api.Tests.Acceptance.Tests.Hooks;
+
+[Binding]
+public class TestInitializationHook
+{
+    private readonly IObjectContainer _container;
+
+    public TestInitializationHook(IObjectContainer container)
+    {
+        _container = container;
+    }
+
+    [BeforeScenario]
+    public void RegisterDependencies()
+    {
+        var restClientManager = new RestClientManager();
+        _container.RegisterInstanceAs<IRestClientManager>(restClientManager);
+
+        var petService = new PetService(restClientManager);
+        _container.RegisterInstanceAs<IPetService>(petService);
+    }
+}
