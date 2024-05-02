1fb8a196d11c55528b2fa62764e25ecd6e122fce
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-02 15:23:26 +0200

    define json serialization settings

---
+++ Petstore.Api.Tests.Acceptance/Utils/JsonSettings.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Petstore.Api.Tests.Acceptance.Utils;
+
+public static class JsonSettings
+{
+    public static JsonSerializerSettings DefaultSettings => new()
+    {
+        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        Converters = new List<JsonConverter> { new StringEnumConverter() }
+    };
+}
