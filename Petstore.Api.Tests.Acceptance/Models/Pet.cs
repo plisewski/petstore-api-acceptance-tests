3e3e34842f76f28264e999dec95b61a230dc1fc7
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-02 15:54:18 +0200

    update Pet model

--- Petstore.Api.Tests.Acceptance/Models/Pet.cs
+++ Petstore.Api.Tests.Acceptance/Models/Pet.cs
@@ -1,13 +1,25 @@
 ﻿using Petstore.Api.Tests.Acceptance.Models.Enums;
+using System.Text.Json.Serialization;
 
 namespace Petstore.Api.Tests.Acceptance.Models;
 
 public class Pet
 {
+    [JsonPropertyName("id")]
     public long Id { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-    public Category? Category { get; set; }
+
+    [JsonPropertyName("category")]
+    public Category Category { get; set; } = new Category();
+
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
-    public List<string>? PhotoUrls { get; set; }
-    public List<Tag>? Tags { get; set; }
+
+    [JsonPropertyName("photoUrls")]
+    public List<string> PhotoUrls { get; set; } = new List<string>();
+
+    [JsonPropertyName("tags")]
+    public List<Tag> Tags { get; set; } = new List<Tag>();
+
+    [JsonPropertyName("status")]
     public PetStatus Status { get; set; }
 }
