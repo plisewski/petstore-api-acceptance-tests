3a4ac49872a90c4d11fd19874cad86ed6d092053
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-03 20:56:32 +0200

    move adding headers to extension method

---
+++ Petstore.Api.Tests.Acceptance/Utils/RestRequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+
+namespace Petstore.Api.Tests.Acceptance.Utils;
+
+public static class RestRequestExtensions
+{
+    public static RestRequest AddHeaders(this RestRequest request, IDictionary<string, string>? headers)
+    {
+        if (headers is not null)
+        {
+            foreach (var (key, value) in headers)
+            {
+                request.AddHeader(key, value);
+            }
+        }
+        return request;
+    }
+}
