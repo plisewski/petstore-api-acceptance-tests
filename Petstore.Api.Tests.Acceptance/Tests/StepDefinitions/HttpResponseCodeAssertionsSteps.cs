90f9637288c9633efe90f20fdc72e363def4b53e
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-07 10:26:31 +0200

    implement common step for checking response status code

---
+++ Petstore.Api.Tests.Acceptance/Tests/StepDefinitions/HttpResponseCodeAssertionsSteps.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace Petstore.Api.Tests.Acceptance.Tests.StepDefinitions;
+
+[Binding]
+public class HttpResponseCodeAssertionsSteps
+{
+    private readonly ScenarioContext _scenarioContext;
+
+    public HttpResponseCodeAssertionsSteps(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Then(@"Http status code should be (.*)")]
+    public void ThenHttpStatusCodeShouldBe(HttpStatusCode expectedStatusCode)
+    {
+        _scenarioContext.Get<RestResponse>().StatusCode.Should().Be(expectedStatusCode);
+    }
+}
