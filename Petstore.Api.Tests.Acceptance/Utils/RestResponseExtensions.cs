1c7af81126dd427237c949f217623f22595113f9
Piotr Lisewski <p.lisewski@hotmail.com>
2024-05-07 10:25:37 +0200

    add response deserializer

---
+++ Petstore.Api.Tests.Acceptance/Utils/RestResponseExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace Petstore.Api.Tests.Acceptance.Utils;
+
+public static class RestResponseExtensions
+{
+    public static T GetResponseContentAs<T>(this RestResponse response)
+    {
+        if (response is null)
+        {
+            throw new ArgumentNullException(nameof(response), "The response is null");
+        }
+
+        if (string.IsNullOrWhiteSpace(response.Content))
+        {
+            throw new ArgumentException("Response content is empty or null", nameof(response));
+        }
+
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(response.Content)!;
+        }
+        catch (Exception ex)
+        {
+
+            throw new InvalidOperationException("Deserialization error: Failed to convert response content.", ex);
+        }
+    }
+}
