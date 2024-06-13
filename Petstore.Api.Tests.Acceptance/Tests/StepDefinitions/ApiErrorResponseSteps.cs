edd46a3b6aec73f9a0343d9cb613f8aa1b627fe4
Piotr Lisewski <p.lisewski@hotmail.com>
2024-06-13 11:08:06 +0200

    implement generic api error response steps

---
+++ Petstore.Api.Tests.Acceptance/Tests/StepDefinitions/ApiErrorResponseSteps.cs
@@ -0,0 +1,22 @@
+﻿using Petstore.Api.Tests.Acceptance.Models;
+using Petstore.Api.Tests.Acceptance.Utils;
+using RestSharp;
+using TechTalk.SpecFlow;
+
+namespace Petstore.Api.Tests.Acceptance.Tests.StepDefinitions;
+
+[Binding]
+public class ApiErrorResponseSteps
+{
+    private readonly ScenarioContext _scenarioContext;
+
+    public ApiErrorResponseSteps(ScenarioContext scenarioContext) => _scenarioContext = scenarioContext;
+
+    [Then(@"The response body contains message: (.*)")]
+    public void ResponseBodyContainsMessage(string errorMessage)
+    {
+        var errorApiResponse = _scenarioContext.Get<RestResponse>().GetResponseContentAs<ApiResponseModel>();
+        errorApiResponse.Should().NotBeNull();
+        errorApiResponse.Message.Should().Be(errorMessage);
+    }
+}
