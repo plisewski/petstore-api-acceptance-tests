3987a461c81b69dabe9e9c5485a25225e63b0e84
Piotr Lisewski <p.lisewski@hotmail.com>
2024-06-13 10:53:42 +0200

    add model for api response

---
+++ Petstore.Api.Tests.Acceptance/Models/ApiResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Petstore.Api.Tests.Acceptance.Models;
+
+public class ApiResponseModel
+{
+    [JsonPropertyName("code")]
+    public int Code { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = default!;
+
+    [JsonPropertyName("message")]
+    public string Message { get; set; } = default!;
+}
